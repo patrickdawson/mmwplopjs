b58c67a7d7aadc39fbb4568f42551596580dd3cd
Patrick Dawson <patrick.dawson@zwickroell.com>
2019-08-28 09:06:45 +0200

    Initial import

---
+++ plopfile.js
@@ -0,0 +1,30 @@
+"use strict";
+
+module.exports = (plop) => {
+    plop.setGenerator("module", {
+        description: "Create a new module with tests",
+
+        // inquirer prompts
+        prompts: [
+            {
+                type: "input",
+                name: "name",
+                message: "Module name?",
+            },
+        ],
+
+        // actions to perform
+        actions: [
+            {
+                type: "add",
+                path: "src/{{camelCase name}}.js",
+                templateFile: "templates/module.hbs",
+            },
+            {
+                type: "add",
+                path: "test/{{name}}.spec.js",
+                templateFile: "templates/module.spec.hbs",
+            },
+        ],
+    });
+};
